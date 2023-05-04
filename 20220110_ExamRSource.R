371a30212450f6a427ff89e245aa05e1f360ab4f
Jesús <31741030+biosherpa@users.noreply.github.com>
2023-05-04 22:10:55 +0200

    Commit first message

--- 20220110_ExamRSource.R
+++ 20220110_ExamRSource.R
@@ -3,6 +3,8 @@ rm(list=ls())
 # if(!require("car")){install.packages('car')} else {require('car')}
 # # if(!require("psych")){install.packages('psych')} else{require('psych')}
 # if(!require("epiDisplay")){install.packages('epiDisplay')} else {require('epiDisplay')}
+#Newline checking git 4may23
+#2nd Newline checking git 4may23
 
 
 multinom<-function(n,prob){
